f14efbd27728bcf30cd86afe3ef5e1e879825c86
kranjbar <YOUR_GITHUB_EMAIL_ADDRESS>
2016-02-23 12:14:51 -0800

    Angular directives implemented

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -5,5 +5,9 @@ angular
 function DataController() {
   var vm = this;
   vm.minDeposit = 1000;
+  vm.stockTrades = 0;
+  vm.mutualFundTrades = 0;
+  vm.optionTrades = 0;
+  vm.marginUsed = 0;
   vm.brokers = brokers;
 }
