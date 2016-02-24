a8c6f3c2114d43a2d4b25acd1d8a4dbf21ff553b
kranjbar <YOUR_GITHUB_EMAIL_ADDRESS>
2016-02-23 16:41:18 -0800

    D3 functioning

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,13 +1,73 @@
-angular
-  .module('BrokerApp', [])
-  .controller('DataController', DataController);
+$(document).ready(function() {
+  var data = [
+    {name: "Etrade",    value:  60},
+    {name: "Scottrade",    value:  80},
+    {name: "Fidelity",     value: 150},
+    {name: "TradeKing",   value: 160},
+    {name: "Charles Schwab", value: 230}
+  ];
 
-function DataController() {
-  var vm = this;
-  vm.minDeposit = 1000;
-  vm.stockTrades = 0;
-  vm.mutualFundTrades = 0;
-  vm.optionTrades = 0;
-  vm.marginUsed = 0;
-  vm.brokers = brokers;
-}
+  function drawBarChart(hard_coded_data) {
+
+    var margin = {top: 20, right: 30, bottom: 30, left: 40},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+    var x = d3.scale.ordinal()
+        .rangeRoundBands([0, width], .1);
+
+    var y = d3.scale.linear()
+        .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .ticks(10, "$");
+
+    var chart = d3.select(".chart")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    x.domain(hard_coded_data.map(function(d) { return d.name; }));
+    y.domain([0, d3.max(hard_coded_data, function(d) { return d.value; })]);
+
+    chart.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    chart.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    chart.selectAll(".bar")
+        .data(hard_coded_data)
+      .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d.name); })
+        .attr("y", function(d) { return y(d.value); })
+        .attr("height", function(d) { return height - y(d.value); })
+        .attr("width", x.rangeBand());
+
+    chart.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+      .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Cost");
+
+  }
+
+  drawBarChart(data);
+
+});
